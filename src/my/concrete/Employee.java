ade702e24ac0103d17733aaa7504e26c9e67c99b
Ryan <Ryan@192.168.0.2>
2014-09-02 10:20:36 -0500

    Concrete "Employee" class built.

--- src/my/concrete/Employee.java
+++ src/my/concrete/Employee.java
@@ -11,5 +11,33 @@ package my.concrete;
  * @author Ryan
  */
 public class Employee {
+    private String empId;
+    private String fName;
+    private String lName;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+    
     
 }
