2029e2b6fffa2d50aa2ada7137ecdf96ff75c5b7
Ryan <Ryan@192.168.0.2>
2014-09-02 11:08:46 -0500

    Concrete Test1

--- src/my/concrete/EmpMain.java
+++ src/my/concrete/EmpMain.java
@@ -14,6 +14,18 @@ public class EmpMain {
     
 
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Scanner keyboard = New Scanner(System.in);
+        
+        HourlyEmployee Emp1 = new HourlyEmployee();
+        Emp1.setAge(24);
+        Emp1.setfName("Bob");
+        Emp1.setlName("Parks");
+        Emp1.setEmpId("123abc");
+        
+        Emp1.setHourlyRate(20.0);
+        Emp1.setHoursWorked(38);
+        
+        
+        System.out.println("Pay is " + Emp1.getTotalPay());
     }
 }
