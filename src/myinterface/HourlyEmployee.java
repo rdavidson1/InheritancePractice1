00cac3f6fd07142b2f8da737ccb43fb4c9be90b0
Ryan <Ryan@192.168.0.2>
2014-09-02 12:03:14 -0500

    Interface Hourly Emp Complete

--- src/myinterface/HourlyEmployee.java
+++ src/myinterface/HourlyEmployee.java
@@ -10,6 +10,69 @@ package myinterface;
  *
  * @author Ryan
  */
-public class HourlyEmployee implements Employee{
+public abstract class HourlyEmployee implements Employee{
+    private String fName;
+    private String lName;
+    private String empId;
+    private int Age;
+    private double hourlyRate;
+    private double hoursWorked;
+    
+    public double getHPay() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    @Override
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    @Override
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    @Override
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+    
     
 }
